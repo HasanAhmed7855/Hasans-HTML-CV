0df2d93d183c59521c6300e55d9326c4ff3692d4
HasanAhmed7855 <63418702+HasanAhmed7855@users.noreply.github.com>
2020-04-15 20:02:22 +0100

    Add files via upload

---
+++ Hasan's website/Hasan's CV.js
@@ -0,0 +1,24 @@
+function WEFunction() {
+    var ScrollElement = document.getElementById("IDWE");
+    ScrollElement.scrollIntoView();
+}
+
+function AFunction() {
+    var ScrollElement = document.getElementById("IDA");
+    ScrollElement.scrollIntoView();
+}
+
+function SFunction() {
+    var ScrollElement = document.getElementById("IDS");
+    ScrollElement.scrollIntoView();
+}
+
+function HFunction() {
+    var ScrollElement = document.getElementById("IDH");
+    ScrollElement.scrollIntoView();
+}
+
+function EFunction() {
+    var ScrollElement = document.getElementById("IDE");
+    ScrollElement.scrollIntoView();
+}
